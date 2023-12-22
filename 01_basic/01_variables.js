9d62fcba539ddfcb4fe8115a07847d4186ac52e0
Lucky rawal <154523932+luckyrawal0001@users.noreply.github.com>
2023-12-22 05:15:57 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountemail = "luckyrawal@google.com"
+var accountpassword = "1122"
+accountcity = "panipat"
+let accountstate = "haryana"
+
+
+// accountId = 2 // not allowed
+
+accountemail = "janvi@google.com"
+accountpassword = "3344"
+accountcity = "karnal"
+
+
+/*
+prefer not to use var
+because of the isue in block scope and funcational scope
+*/
+
+
+
+
+console.log(accountId);
+console. table ([ accountId, accountemail, accountpassword, accountcity, accountstate])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as new version
+
+console. log("lucky rawal")
+
+
+let name = "lucky rawal"
+let age = 17
+let isloggedin = false
+
+
+// number=> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//  null => standalone value
+//  undefiend => 
+// symbol => unique  
+
+
+
+// 

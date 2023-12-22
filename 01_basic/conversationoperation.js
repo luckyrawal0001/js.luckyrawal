4491b3fb6fb732c5ceed1bbd37dea69fccc1b0d7
Lucky rawal <154523932+luckyrawal0001@users.noreply.github.com>
2023-12-22 06:39:53 +0000

    onversation of datatypes

---
+++ 01_basic/conversationoperation.js
@@ -0,0 +1,18 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueINumber = Number (score)
+// console.log(valueINumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN NOT a NUMBER
+// true => 1; false =>0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
